a13cacc781e15aa6fcfd03a3e106b2dac76f6e7f
Hüseyin Özgür <huseyinozgur@protonmail.com>
2025-01-11 16:37:40 +0300

    logcollect: add API versioning and middleware

--- uwebfilter.logcollect/cmd/logcollect/main.go
+++ uwebfilter.logcollect/cmd/logcollect/main.go
@@ -8,19 +8,19 @@ import (
 	middleware "uwebfilter.logcollect/pkg/middleware"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{
 		AppName: "uwebfilter.logcollect",
 		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
-			return ctx.Status(400).JSON(fiber.Map{
-				"success": false,
-			})
+			return ctx.SendStatus(fiber.StatusNotFound)
 		},
 	})
 
-	setupRoutes(app)
+	setupApp(app)
 
 	log.Fatal(app.ListenTLS(
 		":3700",
@@ -29,7 +29,16 @@ func main() {
 	))
 }
 
-func setupRoutes(app *fiber.App) {
-	app.Post("/auth", controller.Auth)
-	app.Post("/ingest", middleware.JWTAuth, controller.IngestPOST)
+func setupApp(app *fiber.App) {
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	setupRoutesV1(app)
+}
+
+func setupRoutesV1(app *fiber.App) {
+	v1 := app.Group("/v1")
+
+	v1.Post("/auth", controller.Auth)
+	v1.Post("/ingest", middleware.JWTAuth, controller.IngestPOST)
 }
